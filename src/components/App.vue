2699a93698cb596333199f48d9dd69d4675355de
KatiaLosignore <katialos.kl@gmail.com>
2023-01-20 16:59:53 +0100

    added header

---
+++ src/components/App.vue
@@ -0,0 +1,49 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+export default {
+  name: 'ViteComics',
+  components: { AppHeader }
+}
+
+</script>
+
+<template>
+  <app-header></app-header>
+  <main></main>
+  <footer></footer>
+
+</template>
+
+<style>
+/* Reset */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+/* Generics */
+body {
+  font-family: 'Open Sans', sans-serif;
+  min-height: 100vh;
+  /* background-image: url('./assets/img/jumbotron.jpg');
+  background-position: center;
+  background-repeat: no-repeat; */
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+}
+
+a {
+  color: black;
+  text-decoration: none;
+}
+
+/* Utils */
+.container {
+  margin: 0 auto;
+  max-width: 1200px;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,61 @@
+<script>
+export default {
+    name: 'AppHeader',
+    data() {
+        return {
+            links: [
+                { text: 'Characters', url: '#', current: false },
+                { text: 'Comics', url: '#', current: true },
+                { text: 'Movies', url: '#', current: false },
+                { text: 'TV', url: '#', current: false },
+                { text: 'Games', url: '#', current: false },
+                { text: 'Collectibles', url: '#', current: false },
+                { text: 'Videos', url: '#', current: false },
+                { text: 'Fans', url: '#', current: false },
+                { text: 'News', url: '#', current: false },
+                { text: 'Shop', url: '#', current: false },
+            ]
+        }
+    }
+};
+
+</script>
+
+<template>
+    <header>
+        <figure>
+            <a href="#"><img src="../assets/img/dc-logo.png" alt="logo"></a>
+        </figure>
+        <nav>
+            <ul>
+                <li v-for='link in links' :key="link.text">
+                    <a :href='link.url' v-text="link.text" :class="{ 'active': link.active }"></a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+</template>
+
+<style scoped>
+    ul {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 30px;
+        padding: 0;
+    }
+
+    ul li {
+        list-style-type: none;
+        font-weight: bold;
+    }
+
+    ul li a {
+        padding: 10px;
+    }
+
+    ul li a.active {
+        color: blue;
+    }
+</style>
