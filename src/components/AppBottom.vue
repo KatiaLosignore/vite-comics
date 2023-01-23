bc2bc896afdd36ba53f21cfd9026d1f33310f481
KatiaLosignore <katialos.kl@gmail.com>
2023-01-23 17:08:32 +0100

    added component

---
+++ src/components/AppBottom.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+    name: 'AppBottom'
+};
+
+</script>
+
+<template>
+    <div class="buttons">
+        <div class="load-button">LOAD MORE</div>
+    </div>
+  
+</template>
+
+
+<style  lang="scss" scoped>
+@use '../assets/scss/partials/variables' as *;
+
+.buttons {
+    margin: 25px auto;
+    padding: 14px;
+    width: 160px;
+    border-radius: 18px;
+    font-size: 11px;
+    font-weight: 600;
+}
+.load-button {
+    background-color: $blue;
+    padding: 10px;
+    color: white;
+    text-align: center;
+}
+
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,15 +1,31 @@
 <script>
+import AppBottom from './AppBottom.vue';
 export default {
-    name: 'AppMain'
+    name: 'AppMain',
+    components: { AppBottom },
+    props: {
+        cards: Array
+    }
 };
 
 </script>
 
 <template>
     <main id="comics">
+        <section id="jumbutron">
+        </section>
         <div class="container">
-            <section>
-                <h2>Content goes here</h2>
+            <section id="cards">
+                
+                <ul>
+                    <li v-for="card in cards" :key="card.series">
+                        <img :src="card.thumb" :alt="card.type">
+                        <p>{{ card.series }}</p>
+                    </li>
+                </ul>
+
+                <app-bottom></app-bottom>
+            
             </section>
         </div>
     </main>
@@ -19,15 +35,50 @@ export default {
 
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables' as *;
+@use '../assets/scss/partials/mixins' as *;
 
     #comics {
         background-color: $black;
-        height: 110px;
+        height: 960px;
     }
 
-    h2{
-        color: $white;
-        padding: 35px;
+   #cards {
+    padding-top: 30px;
+   }
+
+    ul {
+        @include center_flex;
+        flex-wrap: wrap;
+        margin: 8px 0;
+        gap: 15px;
+
+        li {
+            list-style-type: none;
+            font-weight: bold;
+            font-size: 13px;
+            color: $white;
+            text-transform: uppercase;
+            width: 15%;
+            margin: 10px 0;
+        }
+
+            img {
+                width: 200px;
+                height: 200px;
+            }
+
+            p {
+                font-size: 10px;
+                margin-top: 8px;
+            }
+           
+    }
+
+    #jumbutron {
+        background-image: url('../assets/img/jumbotron.jpg');
+        background-size: cover;
+        background-position: top;
+        height: 320px;
     }
 
 </style>
